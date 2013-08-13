770a8fd4555ebdbacde48b9db40bda6184322400
Christian Cable <christiancable@gmail.com>
2013-08-13 20:27:14 +0100

    Updates to fix some IE Issues
    A fix for some of the worst IE issues. Removed grad in the header for IE

--- header.php
+++ header.php
@@ -22,9 +22,8 @@
 <html <?php language_attributes(); ?>>
 <!--<![endif]-->
 <head>
-<link rel="shortcut icon" href="<?php bloginfo('stylesheet_directory'); ?>/favicon.png" />
-<link rel="apple-touch-icon" href="<?php bloginfo('stylesheet_directory'); ?>/apple-touch-icon.png"/>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
+
 <!-- <meta name="viewport" content="width=device-width" /> -->
 
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -53,11 +52,19 @@
 
 	?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
+
+<!--[if lt IE 9]>
+	<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri() ?>/../twentyeleven/style.css" />		
+<![endif]-->
+
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
-
-
-
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+<!--[if lt IE 9]>
+
+	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
+	<script type='text/javascript' src='<?php echo get_stylesheet_directory_uri() ?>/js/respond.min.js'></script>
+<![endif]-->
+
 
 <?php
 	/* We add some JavaScript to pages with the comment form
@@ -86,23 +93,36 @@
 			<?php
 				// Check to see if the header image has been removed
 				$header_image = get_header_image();
-				if ( ! empty( $header_image ) ) :
-			?>
-			<!--  link header image to the gallery rather than the homepage  
-			 <a href="<?php echo esc_url( home_url( '/' ) . get_permalink( 285 ) ); ?>">  -->
-			<a href="<?php echo esc_url( get_permalink( 285 ) ); ?>"> 
-		
+				if ( $header_image ) :
+					// Compatibility with versions of WordPress prior to 3.4.
+					if ( function_exists( 'get_custom_header' ) ) {
+						// We need to figure out what the minimum width should be for our featured image.
+						// This result would be the suggested width if the theme were to implement flexible widths.
+						$header_image_width = get_theme_support( 'custom-header', 'width' );
+					} else {
+						$header_image_width = HEADER_IMAGE_WIDTH;
+					}
+					?>
+			<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
 				<?php
 					// The header image
 					// Check if this is a post or page, if it has a thumbnail, and if it's a big one
-					if ( is_singular() &&
-							has_post_thumbnail( $post->ID ) &&
-							( /* $src, $width, $height */ $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), array( HEADER_IMAGE_WIDTH, HEADER_IMAGE_WIDTH ) ) ) &&
-							$image[1] >= HEADER_IMAGE_WIDTH ) :
+					if ( is_singular() && has_post_thumbnail( $post->ID ) &&
+							( /* $src, $width, $height */ $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), array( $header_image_width, $header_image_width ) ) ) &&
+							$image[1] >= $header_image_width ) :
 						// Houston, we have a new header image!
 						echo get_the_post_thumbnail( $post->ID, 'post-thumbnail' );
-					else : ?>
-					<img id="headerimage" src="<?php header_image(); ?>" width="<?php echo HEADER_IMAGE_WIDTH; ?>" height="<?php echo HEADER_IMAGE_HEIGHT; ?>" alt="" />
+					else :
+						// Compatibility with versions of WordPress prior to 3.4.
+						if ( function_exists( 'get_custom_header' ) ) {
+							$header_image_width  = get_custom_header()->width;
+							$header_image_height = get_custom_header()->height;
+						} else {
+							$header_image_width  = HEADER_IMAGE_WIDTH;
+							$header_image_height = HEADER_IMAGE_HEIGHT;
+						}
+						?>
+					<img src="<?php header_image(); ?>" width="<?php echo $header_image_width; ?>" height="<?php echo $header_image_height; ?>" alt="" />
 				<?php endif; // end check for featured image or standard header ?>
 			</a>
 			<?php endif; // end check for removed header image ?>
@@ -111,7 +131,7 @@
 				// Has the text been hidden?
 				if ( 'blank' == get_header_textcolor() ) :
 			?>
-				<div class="only-search<?php if ( ! empty( $header_image ) ) : ?> with-image<?php endif; ?>">
+				<div class="only-search<?php if ( $header_image ) : ?> with-image<?php endif; ?>">
 				<?php get_search_form(); ?>
 				</div>
 			<?php
@@ -122,17 +142,12 @@
 
 			<nav id="access" role="navigation">
 				<h3 class="assistive-text"><?php _e( 'Main menu', 'twentyeleven' ); ?></h3>
-				<?php /*  Allow screen readers / text browsers to skip the navigation menu and get right to the good stuff. */ ?>
+				<?php /* Allow screen readers / text browsers to skip the navigation menu and get right to the good stuff. */ ?>
 				<div class="skip-link"><a class="assistive-text" href="#content" title="<?php esc_attr_e( 'Skip to primary content', 'twentyeleven' ); ?>"><?php _e( 'Skip to primary content', 'twentyeleven' ); ?></a></div>
 				<div class="skip-link"><a class="assistive-text" href="#secondary" title="<?php esc_attr_e( 'Skip to secondary content', 'twentyeleven' ); ?>"><?php _e( 'Skip to secondary content', 'twentyeleven' ); ?></a></div>
-				<?php /* Our navigation menu.  If one isn't filled out, wp_nav_menu falls back to wp_page_menu. The menu assiged to the primary position is the one used. If none is assigned, the menu with the lowest ID is used. */ ?>
-				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'depth' => '1' ) ); ?>
-				
-				
-				
+				<?php /* Our navigation menu. If one isn't filled out, wp_nav_menu falls back to wp_page_menu. The menu assigned to the primary location is the one used. If one isn't assigned, the menu with the lowest ID is used. */ ?>
+				<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
 			</nav><!-- #access -->
-			
-			
 	</header><!-- #branding -->
 
 
